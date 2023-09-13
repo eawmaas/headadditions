4e84703bfd88617c841d92142f9f8557cbb28cc2
Erwin Maas <89970088+eawmaas@users.noreply.github.com>
2023-09-13 13:45:53 +0100

    Update info.php

---
+++ info.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    "name"          => __("Headadditions", "headadditions"),
+    "url"           => "http://chyrplite.net/",
+    "version"       => "2023.09",
+	"description"   => __("Use the &lt;head&gt; to add <mark>scripts</mark> (like analytics- or consent-scripts), overwrite <mark>styles</mark> or create your own which you can use in your blogposts and improve <mark>SEO</mark>, see README.md for recommendations and instructions.", "headadditions"),
+    "author"        => array(
+        "name"      => "Erwin Maas",
+        "url"       => "https://exdomo.com/"
+    )
+);
