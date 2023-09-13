38d6e43ac581e70d40e36648fadd188a66279643
Erwin Maas <89970088+eawmaas@users.noreply.github.com>
2023-09-13 13:47:26 +0100

    Update headadditions_seo.php

---
+++ headadditions_seo.php
@@ -0,0 +1,65 @@
+<meta property="og:type" content="website">
+
+<meta name="robots" content="index, follow">
+
+<!-- Begin Modifications Recommended -->
+<meta name="author" content="Chyrp Lite Blog User">
+<meta itemprop="author" content="Chyrp Lite Blog User">
+
+<meta name="publisher" content="https://chyrplite.net/">
+<meta itemprop="publisher" content="https://chyrplite.net/">
+
+<meta name="keywords" content="Chyrp Lite, blog, guestbook, markdown, twig, php, github, repository, article, blogpost, comments, likes, usergroups, archive, html5, lightweight, open source, languages, easy installation, websafe">
+<meta itemprop="keywords" content="Chyrp Lite, blog, guestbook, markdown, twig, php, github, repository, article, blogpost, comments, likes, usergroups, archive, html5, lightweight, open source, languages, easy installation, websafe">
+
+<meta itemprop="name" content="My Chyrp Lite Blog">
+<meta property="og:site_name" content="My Chyrp Lite Blog">
+<meta property="og:title" content="My Chyrp Lite Blog">
+<meta property="og:description" content="My Chyrp Lite Blog">
+<!-- End Modifications Recommended -->
+
+<meta property="og:url" content="<?php echo $_SERVER['SCRIPT_URI'];?>">
+<meta itemprop="url" content="<?php echo $_SERVER['SCRIPT_URI'];?>">
+
+<meta property="og:image:type" content="image/webp">
+
+<link rel="image_src" href="https://<?php
+$exploded_dir = explode('/', dirname(__FILE__));
+echo $_SERVER['SERVER_NAME'];
+echo "/$exploded_dir[4]/$exploded_dir[5]/headadditions/img/opengraph.webp";?>">
+<meta property="og:image:secure_url" content="https://<?php
+$exploded_dir = explode('/', dirname(__FILE__));
+echo $_SERVER['SERVER_NAME'];
+echo "/$exploded_dir[4]/$exploded_dir[5]/headadditions/img/opengraph.webp";?>">
+<meta property="og:image" content="https://<?php
+$exploded_dir = explode('/', dirname(__FILE__));
+echo $_SERVER['SERVER_NAME'];
+echo "/$exploded_dir[4]/$exploded_dir[5]/headadditions/img/opengraph.webp";?>">
+<meta property="og:image:width"  content="1600">
+<meta property="og:image:height"  content="833">
+<meta property="og:image:alt" content="My Chyrp Lite Blog"><!-- Modify! -->
+
+<!-- Remove below if you don't have Twitter or change @mytwittername and My Chyrp Lite Blog -->
+<meta name="twitter:card" content="My Chyrp Lite Blog">
+<meta name="twitter:site" content="@mytwittername">
+<meta name="twitter:title" content="My Chyrp Lite Blog">
+<meta name="twitter:description" content="My Chyrp Lite Blog">
+<meta name="twitter:image" content="https://<?php
+$exploded_dir = explode('/', dirname(__FILE__));
+echo $_SERVER['SERVER_NAME'];
+echo "/$exploded_dir[4]/$exploded_dir[5]/headadditions/img/opengraph.webp";?>">
+<meta name="twitter:image:alt" content="My Chyrp Lite Blog">
+
+<!-- Replace these three images in headadditions/img -->
+<link rel="icon" href="https://<?php
+$exploded_dir = explode('/', dirname(__FILE__));
+echo $_SERVER['SERVER_NAME'];
+echo "/$exploded_dir[4]/$exploded_dir[5]/headadditions/img/favicon.ico";?>">
+<link rel="icon" href="https://<?php
+$exploded_dir = explode('/', dirname(__FILE__));
+echo $_SERVER['SERVER_NAME'];
+echo "/$exploded_dir[4]/$exploded_dir[5]/headadditions/img/favicon.svg\"";?> type="image/svg+xml">
+<link rel="icon" href="https://<?php
+$exploded_dir = explode('/', dirname(__FILE__));
+echo $_SERVER['SERVER_NAME'];
+echo "/$exploded_dir[4]/$exploded_dir[5]/headadditions/img/apple-touch-icon.ico";?>">
